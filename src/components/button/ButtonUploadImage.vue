cc55c590dbc9d31accfe4142089b2db3eb21d194
userCommit <usercommit@gmail.com>
2023-08-31 00:40:59 +0700

    update btn upload image

--- src/components/button/ButtonUploadImage.vue
+++ src/components/button/ButtonUploadImage.vue
@@ -9,9 +9,12 @@ export default {
 
 <template>
   <div class="content-button">
-    <button class="btn-upload">
-      <i data-feather="plus-circle" width="3rem" height="3rem"></i>
-    </button>
+    <label class="community_file">
+
+    </label>
+    <label class="btn-upload">
+      <input type="file" id="upload_file" /><i data-feather="plus-circle" width="3rem" height="3rem"></i>
+    </label>
   </div>
   <p class="line-clamp-2 font-general-semibold text-sm mb-2" style="
         overflow: hidden;
@@ -32,7 +35,7 @@ export default {
   justify-content: center;
 }
 
-button.btn-upload {
+label.btn-upload {
   height: 252px;
   width: 380px;
   background-color: rgba(128, 128, 128, 0.137);
@@ -42,5 +45,10 @@ button.btn-upload {
   align-items: center;
   justify-content: center;
   color: #fff;
+}
+input#upload_file{
+  opacity: 0;
+  z-index: -1;
+  position: absolute;
 }
 </style>
