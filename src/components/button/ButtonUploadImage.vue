7873ccd849ceb05eeb51d0504e3d9b4d4387a7f0
userCommit <usercommit@gmail.com>
2023-08-31 00:32:42 +0700

    update btn upload

--- src/components/button/ButtonUploadImage.vue
+++ src/components/button/ButtonUploadImage.vue
@@ -8,37 +8,39 @@ export default {
 </script>
 
 <template>
-  <div
-    class="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 relative m-16 mt-0 square"
-    aria-label="Single Project"
-  >
-    <button
-      class="rounded-t-xl border-none w-full h-1/2 flex items-center justify-center text-white bg-gray-200"
-      @click="$emit('update:isOpen', true)">
-      <i
-        data-feather="plus-circle"
-        width="3rem"
-        height="3rem"
-      ></i>
+  <div class="content-button">
+    <button class="btn-upload">
+      <i data-feather="plus-circle" width="3rem" height="3rem"></i>
     </button>
   </div>
-  <p
-      class="line-clamp-2 font-general-semibold text-sm mb-2"
-      style="
+  <p class="line-clamp-2 font-general-semibold text-sm mb-2" style="
         overflow: hidden;
         display: -webkit-box;
         -webkit-box-orient: vertical;
         -webkit-line-clamp: 2;
         font-size: 18px;
         margin-bottom: 32px;
-      "
-    >
-      Upload OR choose image form below
-    </p>
+      ">
+    Upload OR choose image form below
+  </p>
 </template>
 
 <style scoped>
-button{
+.content-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+button.btn-upload {
   height: 252px;
+  width: 380px;
+  background-color: rgba(128, 128, 128, 0.137);
+  border-radius: 16px;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
 }
 </style>
