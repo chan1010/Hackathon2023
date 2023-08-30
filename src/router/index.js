603882e01c53a95d524d84ae206328f34e377c74
userCommit <usercommit@gmail.com>
2023-08-31 00:22:53 +0700

    update home

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHistory } from "vue-router";
+// import Home from "../views/Home.vue";
 
 const routes = [
-	{
-		path: '/',
-		name: 'Home',
-		component: Home,
-		meta: {
-			title: 'Hackathon',
-		},
-	},
-	{
-		path: '/check-med',
-		name: 'CheckMedicine',
-		component: () =>
-			import('../views/CheckMedicine.vue'),
-		meta: {
-			title: 'Hackathon - Check Medicine',
-		},
-	},
-	{
-		path: '/med-detail/:id',
-		name: 'MedicineDetail',
-		component: () =>
-			import('../views/DetailMedicine.vue'),
-		meta: {
-			title: 'Hackathon - Medicine Detail',
-		},
-	},
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("../views/CheckMedicine.vue"),
+    meta: {
+      title: "Hackathon - Check Medicine",
+    },
+  },
+  {
+    path: "/check-med",
+    name: "CheckMedicine",
+    component: () => import("../views/CheckMedicine.vue"),
+    meta: {
+      title: "Hackathon - Check Medicine",
+    },
+  },
+  {
+    path: "/med-detail/:id",
+    name: "MedicineDetail",
+    component: () => import("../views/DetailMedicine.vue"),
+    meta: {
+      title: "Hackathon - Medicine Detail",
+    },
+  },
 ];
 
 const router = createRouter({
-	history: createWebHistory(process.env.BASE_URL),
-	routes,
-	scrollBehavior() {
-		document.getElementById('app').scrollIntoView();
-	},
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  scrollBehavior() {
+    document.getElementById("app").scrollIntoView();
+  },
 });
 
 export default router;
